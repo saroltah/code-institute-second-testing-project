f3f84e8aec69a0603b416ddeaa9c2810f1c8d869
Sarolta <sarolta.hegyi@gmail.com>
2024-02-05 17:33:12 +0100

    add pther tests and just enough code to pass

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,6 +2,8 @@
  * @jest-environment jsdom
  */
 
+const { game } = require("../game");
+
 beforeAll(() => {
   let fs = require("fs");
   let fileContents = fs.readFileSync("index.html", "utf-8");
@@ -14,5 +16,16 @@ describe("game object contains correct keys", () => {
   test("score key exists", () => {
     expect("score" in game).toBe(true);
   });
-  npm;
+  test("currentGame key exists", () => {
+    expect("currentGame" in game).toBe(true);
+  });
+  test("playerMoves key exists", () => {
+    expect("playerMoves" in game).toBe(true);
+  });
+  test("choices key exists", () => {
+    expect("choices" in game).toBe(true);
+  });
+  test("choices contain correct ids", () => {
+    expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+  });
 });
