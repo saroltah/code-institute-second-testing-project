25876cc102a6bbdadfa0c57b7cb6954845126135
Sarolta <sarolta.hegyi@gmail.com>
2024-02-06 08:20:16 +0100

    add more functionality with test, and just enough code to pass those tests

--- scripts/game.js
+++ scripts/game.js
@@ -5,4 +5,15 @@ let game = {
   choices: ["button1", "button2", "button3", "button4"],
 };
 
-module.exports = { game };
+function newGame() {
+  game.currentGame = [];
+  game.playerMoves = [];
+  game.score = 0;
+  showScore();
+}
+
+function showScore() {
+  document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore };
